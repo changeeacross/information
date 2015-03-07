fd58c35ddab9153f3b69781fd13a3f1afa3c995e
wuct <wu.chingting@gmail.com>
2015-03-07 17:32:19 +0800

    update tags after info created

--- server/models/info.js
+++ server/models/info.js
@@ -16,7 +16,24 @@ var infoSchema = new mongoose.Schema(
 	, downVote  : { type : Number, required: true, default: 0 }
 	, createdAt : { type : Date, required: true, default : Date.now }
 });
- 
+
+// TODO: refactor this to DAO
+infoSchema.post('save', function (info) {
+	if (info.tags.length == 0) return;
+	var Tag = mongoose.model('Tag');
+	info.tags.map(function (tag) {
+		Tag.update({
+			name: tag,
+		}, {
+			$inc: { count: 1 }
+		}, {
+			upsert: true
+		}, function (err, numOfEffect) {
+			if (err) return console.log(err);
+		});
+	});
+});
+
 var Info = mongoose.model('Info', infoSchema);
 
 module.exports = Info;
