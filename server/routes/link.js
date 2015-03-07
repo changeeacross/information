0c5b52877ed54b27a00a695a4ea27f8420d3c00f
wuct <wu.chingting@gmail.com>
2015-03-07 15:45:05 +0800

    api done

--- server/routes/link.js
+++ server/routes/link.js
@@ -3,9 +3,28 @@
 
 var express = require('express');
 var router = express.Router();
+var MetaInspector = require('node-metainspector');
 
 router.get('/', function(req, res, next) {
-  res.send('index');
+	var url = req.query.url;
+	var client = new MetaInspector(url, {});
+	 
+	client.on("fetch", function(){
+		// console.log(client);
+		var info = {
+			title: client.title,
+			description: client.description,
+			keywords: client.keywords,
+			image: client.image
+		}
+		res.json(info);
+	});
+	 
+	client.on("error", function (err){
+		next(err);
+	});
+	 
+	client.fetch();	 
 });
 
 module.exports = router;
