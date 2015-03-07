0cd9e36a7d82362e41f6da1a2d7761d34fd9f39b
psdmac <psdmac@gmail.com>
2015-03-07 19:40:15 +0800

    submit info

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -1,6 +1,6 @@
 var host = "http://localhost"
 
-myApp = angular.module('myApp', ['ui.router']);
+myApp = angular.module('myApp', ['ui.router','ngTagsInput']);
 
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
@@ -18,19 +18,59 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
 });
 
 
-myApp.controller('SubmitCtrl', ['$scope','$http', function($scope, $http){
+myApp.controller('SubmitCtrl', ['$scope','$http','$timeout', function($scope, $http, $timeout){
   $scope.back = function() {
     history.back();
   }
+  var timer;
   $scope.fetchLink = function(link) {
-    console.log(link);
+    var parseLink = link.split(".");
+    if (parseLink.length < 2) {
+      return;
+    } else {
+      if (timer) {
+        $timeout.cancel(timer);
+      }
+      timer = $timeout(function() {
+        $http({
+          method: 'GET',
+          url: host + '/link?url='+ link
+        }).success(function(data, status, headers, config) {
+          console.log(data);
+          $scope.tags = [];
+          angular.forEach(data.keywords, function(key) {
+            this.push({
+              text: key
+            })
+          }, $scope.tags)
+          $scope.info = data;
+          console.log($scope.tags);
+        }).error(function(argument) {
+          console.log(argument);
+        })  
+      }, 1000);  
+    }
+  }
+  $scope.submitInfo = function(info, tags) {
+    console.log(info, tags);
+    var data = {
+      title: info.title,
+      description: info.description,
+      image: info.image
+    }
+    var tags = [];
+    angular.forEach(tags, function(tag) {
+      tags.push(tag.text); 
+    });
+    data.tags = tags;
     $http({
-      method: 'GET',
-      url: host + '/link?url='+ link
+      method: 'POST',
+      url: host + '/info',
+      data: data
     }).success(function(data, status, headers, config) {
       console.log(data);
     }).error(function(argument) {
-      alert(argument);
-    })
+      console.log(argument);
+    })  
   }
 }])
