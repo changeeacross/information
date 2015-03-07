6e9b9bfcd256d8a2ae587f37aa6246bf8c0acf93
wuct <wu.chingting@gmail.com>
2015-03-07 14:32:01 +0800

    add router

--- server/routes/link.js
+++ server/routes/link.js
@@ -1,9 +1,11 @@
+// link router
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('index');
 });
 
 module.exports = router;
