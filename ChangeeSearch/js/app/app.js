30b7106075d70948e8a3ff1af46961497f547a4f
lulalachen <lulalachen@gmail.com>
2015-03-08 08:46:17 +0800

    rearrange folder

---
+++ ChangeeSearch/js/app/app.js
@@ -0,0 +1,98 @@
+﻿var myApp = angular.module('AngularChromeEx', ['ngTagsInput']);
+
+// var host = "http://localhost"
+
+// // myApp = angular.module('myApp', ['ui.router','ngTagsInput']);
+
+
+// myApp.config(function($stateProvider, $urlRouterProvider) {
+//   $urlRouterProvider.otherwise("/");
+
+//   $stateProvider
+//     .state('home', {
+//       url: "/",
+//       templateUrl: "partials/home.html"
+//     }).state('home.submit', {
+//       url: "submit",
+//       templateUrl: "partials/submit.html",
+//       controller: "SubmitCtrl"
+//     });
+// });
+
+
+// myApp.controller('SubmitCtrl', ['$scope','$http','$timeout', function($scope, $http, $timeout){
+
+// 	$scope.infoData = [];
+
+// 	$http.get('http://localhost/info/read').success(function(data){
+// 		console.log(data);
+// 		angular.forEach(data, function(val){
+// 			$scope.infoData.push(val);
+// 		});
+// 	});
+
+
+// 	var loader = new SVGLoader( document.getElementById( 'loader' ), { speedIn : 300, easingIn : mina.easeinout } );
+// 		$scope.back = function() {
+// 		history.back();
+// 	}
+// 	var timer;
+// 	$scope.fetchLink = function(link) {
+// 		var parseLink = link.split(".");
+// 		if (parseLink.length < 2) {
+// 		  	return;
+// 		} else {
+// 			if (timer) {
+// 				$timeout.cancel(timer);
+// 			}
+// 		  	timer = $timeout(function() {
+// 		    loader.show();
+// 		    $http({
+// 		      method: 'GET',
+// 		      url: host + '/link?url='+ link
+// 		    }).success(function(data, status, headers, config) {
+// 		    console.log(data);
+// 		    $scope.tags = [];
+// 		    angular.forEach(data.keywords, function(key) {
+// 			        this.push({
+// 			          	text: key
+// 			        })
+// 		      	}, $scope.tags)
+// 		      	$scope.info = data;
+// 		      	loader.hide();
+// 		      	console.log($scope.tags);
+// 		    }).error(function(argument) {
+// 		      console.log(argument);
+// 		    })  
+// 	  }, 1000);  
+// 	}
+// 	}
+// 	$scope.submitInfo = function(info, tags) {
+// 	if ($scope.link.search('http') < 0) {
+// 	  $scope.link = 'http://' + $scope.link;
+// 	}
+// 	var data = {
+// 	  title: info.title,
+// 	  description: info.description,
+// 	  image: info.image,
+// 	  link: $scope.link
+// 	}
+// 	var tagArray = [];
+// 	angular.forEach(tags, function(tag) {
+// 	  tagArray.push(tag.text); 
+// 	});
+// 	data.tags = tagArray;
+// 	loader.show();
+// 	$http({
+// 	  method: 'POST',
+// 	  url: host + '/info',
+// 	  data: data
+// 	}).success(function(data, status, headers, config) {
+// 	  console.log(data);
+// 	  loader.hide();
+// 	  history.back();
+// 	}).error(function(argument) {
+// 	  console.log(argument);
+// 	})  
+// 	}
+// }])
