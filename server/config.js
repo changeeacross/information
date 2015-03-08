9b0c999cd510b9a9a1a1b4833be70c7805a28114
wuct <wu.chingting@gmail.com>
2015-03-08 15:32:56 +0800

    add del info api

--- server/config.js
+++ server/config.js
@@ -6,4 +6,9 @@ module.exports = {
 	FB_APPID: process.env.FB_APPID,
 	FB_APPSECRET: process.env.FB_APPSECRET,
 
+	// TODO: implement ACL in user model
+	MASTER_FB_IDS: [
+		"10152860480424387", // wuct
+		'10204138590214598' // mark
+	]
 };
